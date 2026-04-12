81e171ef3baa8248ccf283b71cf93cfaba9ede6a
Mame Yacine Ndiaye <nyacine183@gmail.com>
2026-04-12 02:07:29 +0000

    Référencement des éléments HTML pour interaction JavaScript

---
+++ Javascript/src/js/main.js
@@ -0,0 +1,5 @@
+import "src/output.css"
+
+const conteneur = document.getElementById("conteneur-projets");
+const form = document.getElementById("formulaireProjet");
+const compteur = document.getElementById("compteurProjets");
