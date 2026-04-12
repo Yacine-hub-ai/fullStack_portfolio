8343b39ec9239038b84ebe5c63a234adf2a7e5ca
Mame Yacine Ndiaye <nyacine183@gmail.com>
2026-04-12 02:10:33 +0000

    Déclaration du tableau de stockage des projets en mémoire

--- Javascript/src/js/main.js
+++ Javascript/src/js/main.js
@@ -3,3 +3,5 @@ import "src/output.css"
 const conteneur = document.getElementById("conteneur-projets");
 const form = document.getElementById("formulaireProjet");
 const compteur = document.getElementById("compteurProjets");
+
+let projets = [];
